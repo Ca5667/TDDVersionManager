74cca9546a62d1d6d676a8ef074fbcc130f35a64
cadesign68 <cadesign68@gmail.com>
2024-02-22 10:24:17 +0100

    Rajout Nouveaux tests Minor et Patch

--- src/VersionManager.java
+++ src/VersionManager.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class VersionManager {
+
+import org.junit.jupiter.api.Test;
+
+public  class VersionManager { // On déclare la classe VersionManager
+
+    void majeur(){}; // On crée une coquille vide pour le test Majeur (100)
+
+    void mineur() {}; // On crée une coquille vide pour le test Mineur (10)
+
+    int getVers() { // On crée un test pour la methode getVers()
+        return -1; // on retourne -1
+    };
 }
+
--- src/VersionManagerTest.java
+++ src/VersionManagerTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
-class VersionManagerTest {
-  
+import org.junit.jupiter.api.Test; // On importe la biblioteque de test JUNIT
+import static org.junit.jupiter.api.Assertions.*; // On importe la bibliotheque ASSERTIONS
+
+class VersionManagerTest { // On crée une classe VersionManagerTest
+
+
+     @Test // Assert de Test
+    void majeur() { // On met en place la methode majeur
+         VersionManager instance = new VersionManager(); // On instancie la class VersionManager
+            assertEquals(100, instance.getVers()); // Test Majeur = 100
+        }
+
+
+    @Test // Assert de Test
+    void mineur() {
+        VersionManager instance = new VersionManager();
+        assertEquals(10, instance.getVers());
+    }
+
+    @Test // Assert de Test
+    void getVers() {
+        VersionManager instance = new VersionManager();
+        assertEquals(1, instance.getVers());
+    }
 }
